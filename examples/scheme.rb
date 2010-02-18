398737f7e9d555ee06bb18c5db60fabab2eb37ca
luikore <usurffx@gmail.com>
2010-02-18 22:15:56 +0800

    make the scheme interpreter shorter (for shocking effect when I post it on reddit)

--- examples/scheme.rb
+++ examples/scheme.rb
@@ -1,11 +1,11 @@
 # A simple scheme interpreter
-
 require "rsec"
 
 class Scheme
   include Rsec::Helpers
 
   def initialize
+    spacee  = /\s*/.r.skip
     bra     = /\(\s*/.r.skip
     ket     = /\s*\)/.r.skip
     boolean = /\#[tf]/.    r.map {|n| ValueNode[n=='#t'] }
@@ -19,26 +19,19 @@ class Scheme
     list    = nil # declare for lazy
     cell    = atom | lazy{list}
     list    = (   bra < cell.join(spacee)._? < ket   ).map {|(n)| ListNode[*n] }
-    @parser = (spacee < cell.join(spacee)._? < spacee).map {|(n)| ListNode[*n] }
+    cells   = (spacee < cell.join(spacee)._? < spacee).map {|(n)| ListNode[*n] }
+    @parser = cells.eof
   end
-      
+
   def run source
-    @ctx = Rsec::ParseContext.new source, 'scm'
-    res = @parser._parse @ctx
-    if !res or !@ctx.eos?
-      raise Rsec::ParseError['syntax error', @ctx]
-    end
+    res = @parser.parse! source
     res.eval Runtime.new
   end
   
   ValueNode = Struct.new :val
   class ValueNode
-    def eval *xs
-      val
-    end
-    def pretty_print q
-      q.text "<#{val}>"
-    end
+    def eval *xs; val; end
+    def pretty_print q; q.text "<#{val}>"; end
   end
   
   class ListNode < Array
@@ -58,12 +51,9 @@ class Scheme
     def initialize parent = {}
       @parent = parent
     end
-    
     def [] key
       super(key) || @parent[key]
     end
-    
-    # define a function
     def define id, &p
       self[id] = proc do |bind, xs|
         p[* xs.map{|x| x.eval bind }]
@@ -77,47 +67,34 @@ class Scheme
       
       self['define'] = proc do |bind, (param, body)|
         case param
-        # (define (name plist[0]) body)
         when ListNode
           func, *xs = param
           self[func] = self['lambda'][bind, [xs, body]]
-        # (define param body)
         when String
           self[param] = body.eval bind
         end
       end
       
-      # declare:
-      #   (lambda (xs[0] xs[1]) body)
+      # declare: (lambda (xs[0] xs[1]) body)
       self['lambda'] = proc do |bind_def, (xs, body)|
         xs = [xs] if xs.is_a?(String)
         new_bind = Bind.new bind_def
-        # calling:
-        #   (some vs[0] vs[1])
+        # calling: (some vs[0] vs[1])
         proc do |bind_call, vs|
           vs = vs.map{|v| v.eval bind_call}
-          new_bind.merge! Hash[xs.zip vs]
-          body.eval new_bind
+          body.eval new_bind.merge(Hash[xs.zip vs])
         end
       end
       
-      # lazy (short cut)
       self['if'] = proc do |bind, (p, left, right)|
         p.eval(bind) ? left.eval(bind) : right.eval(bind)
       end
       
-      # misc
-      %w|+ - * / ** % > <|.each do |s|
-        define s, &s.to_sym
-      end
+      %w|+ - * / ** % > <|.each{|s| define s, &s.to_sym }
       define '=', &:==
-      define 'display' do |x|
-        puts x
-      end
+      define('display'){|x| puts x}
     end
   end
 end
 
-s = Scheme.new
-s.run File.read ARGV[0]
-
+Scheme.new.run File.read ARGV[0]
