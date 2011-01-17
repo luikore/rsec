f7b48ae014684a8dc6fba6d4c373ceeeb221ca72
luikore <usurffx@gmail.com>
2011-01-17 15:43:06 +0800

    improve the simple scheme interpreter to simple-as-shit shceme interpreter

--- examples/scheme.rb
+++ examples/scheme.rb
@@ -1,97 +1,84 @@
-# A simple scheme interpreter
+# A simple-as-shit scheme interpreter
 require "rsec"
 
 class Scheme
   include Rsec::Helpers
 
-  def initialize
-    spacee  = /\s*/.r.skip
-    boolean = /\#[tf]/.    r {|n| ValueNode[n=='#t'] }
-    integer = /0|[1-9]\d*/.r {|n| ValueNode[n.to_i]  }
-    id      = /[^\s\(\)\[\]]+/.r {|n|
-                def n.eval bind, *xs
-                  bind[self]
-                end
-                n
-              }
-    atom    = boolean | integer | id
-    cell    = atom | lazy{list}
-    list    = spacee.join(cell).wrap('()').map {|n| ListNode[*n] }
-    cells   = spacee.join(cell).map {|n| ListNode[*n] }
-    @parser = cells.eof
-  end
+  Value = Struct.new :val
 
-  def run source
-    res = @parser.parse! source
-    res.eval Runtime.new
-  end
-  
-  ValueNode = Struct.new :val
-  class ValueNode
-    def eval *xs; val; end
-    def pretty_print q; q.text "<#{val}>"; end
-  end
-  
-  class ListNode < Array
-    def eval bind
-      head, *tail = self
-      case head
-      when String
-        pr = bind[head]
-        pr.is_a?(Proc) ? pr[bind, tail] : pr
-      when ListNode
-        map{|n| n.eval bind }.last
-      end
-    end
-  end
-  
   class Bind < Hash
     def initialize parent = {}
       @parent = parent
     end
+
+    def define id, &p
+      self[id] = proc do |bind, xs|
+        p[* xs.map{|x| bind.eval x }]
+      end
+    end
+
+    def eval node
+      case node
+      when Value;  node.val
+      when String; self[node]
+      when Array
+        head, *tail = node
+        case head
+        when String
+          pr = self[head]
+          pr.is_a?(Proc) ? pr[self, tail] : pr # invoke lambda
+        when Array
+          node.map{|n| self.eval n }.last # sequence execution
+        end
+      end
+    end
+
     def [] key
       super(key) || @parent[key]
     end
-    def define id, &p
-      self[id] = proc do |bind, xs|
-        p[* xs.map{|x| x.eval bind }]
-      end
-    end
   end
 
-  class Runtime < Bind
-    def initialize
-      super()
-      
-      self['define'] = proc do |bind, (param, body)|
-        case param
-        when ListNode
-          func, *xs = param
-          self[func] = self['lambda'][bind, [xs, body]]
-        when String
-          self[param] = body.eval bind
-        end
+  def initialize
+    spacee  = /\s*/.r.skip
+    boolean = /\#[tf]/.    r {|n| Value[n=='#t'] }
+    integer = /0|[1-9]\d*/.r {|n| Value[n.to_i]  }
+    id      = /[^\s\(\)\[\]]+/.r
+    atom    = boolean | integer | id
+    cell    = atom | lazy{list}
+    list    = spacee.join(cell).wrap('()')
+    cells   = spacee.join(cell)
+    @parser = cells.eof
+
+    @vm = Bind.new
+    @vm['define'] = -> bind, (param, body) {
+      if param.is_a?(String)
+        @vm[param] = bind.eval body
+      else
+        func, *xs = param
+        @vm[func] = @vm['lambda'][bind, [xs, body]]
       end
-      
-      # declare: (lambda (xs[0] xs[1]) body)
-      self['lambda'] = proc do |bind_def, (xs, body)|
-        xs = [xs] if xs.is_a?(String)
+    }
+    # declare: (lambda (xs[0] xs[1]) body)
+    @vm['lambda'] = -> bind_def, (xs, body) {
+      xs = [xs] if xs.is_a?(String)
+      # calling: (some vs[0] vs[1])
+      -> bind_call, vs {
+        vs = vs.map{|v| bind_call.eval v }
         new_bind = Bind.new bind_def
-        # calling: (some vs[0] vs[1])
-        proc do |bind_call, vs|
-          vs = vs.map{|v| v.eval bind_call}
-          body.eval new_bind.merge(Hash[xs.zip vs])
-        end
-      end
-      
-      self['if'] = proc do |bind, (p, left, right)|
-        p.eval(bind) ? left.eval(bind) : right.eval(bind)
-      end
-      
-      %w|+ - * / ** % > <|.each{|s| define s, &s.to_sym }
-      define '=', &:==
-      define('display'){|x| puts x}
-    end
+        xs.zip(vs){|x, v| new_bind[x] = v }
+        new_bind.eval body
+      }
+    }
+    @vm['if'] = -> bind, (p, left, right) {
+      bind.eval(bind.eval(p) ? left : right)
+    }
+    %w|+ - * / ** % > <|.each{|s| @vm.define s, &s.to_sym }
+    @vm.define '=', &:==
+    @vm.define('display'){|x| puts x}
+  end
+
+  def run source
+    @vm.eval @parser.parse! source
   end
 end
 
